311c5104221c63faf9fc9649f24aeac00ff84f0a
DESKTOP-3POO2SB\sunho <tjsgh9414@naver.com>
2022-05-03 15:39:09 +0900

    회원가입 및 로그인

--- join.php
+++ join.php
@@ -5,7 +5,7 @@
     <div id="regist_wrap" class="wrap">
         <div>
             <h1>Regist Form</h1>
-            <form action="join_ok.php" method="post" name="regiform" id="regist_form" class="form" onsubmit="return sendit()">
+            <form action="join_ok.php" method="post" name="regiform" id="regist_form" class="form">
                 <p><input type="text" name="userid" id="userid" placeholder="ID"></p>
                 <p><input type="password" name="userpw" id="userpw" placeholder="Password"></p>
                 <p><input type="password" name="userpw_ch" id="userpw_ch" placeholder="Password Check"></p>
@@ -13,7 +13,7 @@
                 <p><input type="text" name="useremail" id="useremail" placeholder="E-mail"></p>
            
                 <p><input type="submit" value="Sin Up" class="signup_btn"></p>
-                <p class="pre_btn">Are you join? <a href="index.php">Login.</a></p>
+                <p class="pre_btn">Are you join? <a href="login.php">Login.</a></p>
             </form>
         </div>
     </div>
--- login.php
+++ login.php
@@ -8,7 +8,7 @@
         <form action="login_ok.php" method="post" id="login_form">
             <p><input type="text" name="userid" id="userid" placeholder="ID"></p>
             <p><input type="password" name="userpw" id="userpw" placeholder="Password"></p>
-            <p><input type="submit" value="Login" class="login_btn"></p>
+            <p><input type="button" value="Login" class="login_btn"><a href="login_ok.php"></a></p>
         </form>
         <p class="regist_btn">Not a member? &nbsp;<a href="join.php">Sign Up</a></p>
     </div>
