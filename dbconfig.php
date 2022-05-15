26e059b65ac41fcd25c1ba8f1afe670c35bef0fd
DESKTOP-3POO2SB\sunho <tjsgh9414@naver.com>
2022-05-15 19:07:33 +0900

    db 로컬호스트

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,9 @@
 <?php
-    $conn = mysqli_connect("localhost", "sunho", "1234", "frontdb");
+    $conn = mysqli_connect("127.0.0.1", "sunho", "1234", "frontdb");
     $conn->set_charset("utf8");
 
-    if(!$conn) {
-        echo "실패";
-    } 
+    // if(!$conn) {
+    //     echo "실패";
+    // } else {
+    //     echo "성공";
+    // }
