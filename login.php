3a8be8c7f39310f4d3bf314f3cb78e1c1742a6aa
DESKTOP-3POO2SB\sunho <tjsgh9414@naver.com>
2022-05-15 19:29:16 +0900

    로그인 페이지 구현중

--- login.php
+++ login.php
@@ -1,16 +1,33 @@
+<?php
+    include_once "common.php";
+    include_once "dbconfig.php";
+?>
 <head>
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div id="login_wrap">
-    <div>
-        <h1>로그인을 해주세요</h1>
-        <form action="login_ok.php" method="post" id="login_form">
-            <p><input type="text" name="userid" id="userid" placeholder="ID"></p>
-            <p><input type="password" name="userpw" id="userpw" placeholder="Password"></p>
-            <p><input type="button" value="Login" class="login_btn"><a href="login_ok.php"></a></p>
-        </form>
-        <p class="regist_btn">Not a member? &nbsp;<a href="join.php">Sign Up</a></p>
+    <div style="text-align:center;">
+    
+            <h1>로그인을 해주세요</h1><br>
+            <form action="login_ok.php" method="post" id="login_form">
+                <p><input type="text" name="userid" id="userid" placeholder="ID" style="width:150px;">
+                <!-- <input type="button" value="중복확인" onclick="validationChk()";> -->
+                </p><br>
+                <p><input type="password" name="userpw" id="userpw" placeholder="Password"></p><br>
+                <p><input type="button" value="Login" class="login_btn"><a href="login_ok.php"></a></p>
+            </form>
+            <h3>회원이 아니신가요? <input type="button" value="회원가입" onclick="location.href='join.php'"></h3>
     </div>
 </div>
 </body>
+<script>
+    $(document).ready(function() {
+        // validationChk();
+    })
+
+    // function validationChk() {
+    //     var user_id = $("#userid").val();
+        
+    // }
+</script>
