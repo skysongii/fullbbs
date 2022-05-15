9f2ef872dacaf5cc8dfd16f11dd6542bd7b1f1ae
DESKTOP-3POO2SB\sunho <tjsgh9414@naver.com>
2022-05-15 19:08:00 +0900

    인덱스 날짜 출력 변경 및 로그인 버튼, 글쓰기 버튼 추가

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
     // $row = $select_result->fetch_assoc();
 	$select_result = mysqli_query($conn, $select_query);
 	$row = mysqli_fetch_array($select_result);
-	// echo $row['title'];
-	
 	?>
 <!doctype html>
 <head>
@@ -19,10 +17,13 @@
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-<div id="board_area"> 
-<h1>자유게시판</h1>
-<h4>자유롭게 글을 쓸 수 있는 게시판입니다.</h4>
-	<table class="list-table">
+	<div id="board_area"> 
+		<h1>자유게시판</h1>
+		<h4>자유롭게 글을 쓸 수 있는 게시판입니다.</h4>
+		<div style="float: right;">
+			<p><input type="button" value="로그인" class="login_btn"><a href="login_ok.php"></a></p>
+		</div>
+		<table class="list-table">
 	<thead>
 		<tr>
 			<th width="70">번호</th>
@@ -30,46 +31,40 @@
 				<th width="120">글쓴이</th>
 				<th width="100">작성일</th>
 				<th width="100">조회수</th>
-
-				<input type="button" value="글쓰기" onclick="location.href='write.php'">
 			</tr>
 		</thead>
-
-		<?php
-			while($row = mysqli_fetch_array($select_result))
-			{
-				//title변수에 DB에서 가져온 title을 선택
-				echo "<script>alert('$row')</script>";
-				$seq = $row['seq'];
-				$title = $row['title'];
-				$writer = $row['writer'];
-				$write_date = $row['write_date'];
-				$hits = $row['hits'];
-				if(strlen($title)>30)
-				{ 
-					//title이 30을 넘어서면 ...표시
-					$title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
-				}
-				?>
-
-	<tbody>
 		<tr>
-			<?= $seq;?>
-		<td width="70"><?php echo $seq; ?></td>
-		<td width="500"><a href=""><?php echo $title;?></a></td>
-		<td width="120"><?php echo $writer?></td>
-		<td width="100"><?php echo $write_date?></td>
-		<td width="100"><?php echo $hits; ?></td>
-		</tr>
-	</tbody>
-	<p><input type="button" value="Login" class="login_btn"><a href="login_ok.php"></a></p>
-	<?php } ?>
-	<!-- <input type=button value="글쓰기"><a href="/page/board/write.php"><button>글쓰기</button></a> -->
+			<?php
+				while($row = mysqli_fetch_array($select_result))
+				{
+					//title변수에 DB에서 가져온 title을 선택
+					$seq = $row['seq'];
+					$title = $row['title'];
+					$writer = $row['writer'];
+					$write_date = $row['write_date'];
+					$w_date = substr($write_date, 2, 14);
+					$hits = $row['hits'];
 
+					if(strlen($title)>30)
+					{ 
+						//title이 30을 넘어서면 ...표시
+						$title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
+					}
+					?>
+			<td width="70"><?= $seq; ?></td>
+			<td width="500"><a href=""><?php echo $title;?></a></td>
+			<td width="120"><?php echo $writer?></td>
+			<td width="100"><?php echo $w_date?></td>
+			<td width="100"><?php echo $hits; ?></td>
+		</tr>
+		<?php } ?>
+		<!-- <input type=button value="글쓰기"><a href="/page/board/write.php"><button>글쓰기</button></a> -->
+		
 	</table>
-	<!-- <div id="write_btn"> -->
-	<!-- </div> -->
-</div>
+	<div style="float: right;">
+	<br>
+		<input type="button" value="글쓰기" onclick="location.href='write.php'">
+	</div>
 <!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> -->
 </body>
 </html>
