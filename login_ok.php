fb435210f195649ad1b8b44c257d6e8f7bab4a73
DESKTOP-3POO2SB\sunho <tjsgh9414@naver.com>
2022-05-17 10:41:42 +0900

    테스트

--- login_ok.php
+++ login_ok.php
@@ -13,10 +13,9 @@
     if($mb_id != '' || $mb_pw != '') {
 
         if($row_mb_id == $mb_id && $row_mb_pw == $mb_pw) { ?>
-            <!-- <script>alert("로그인에 성공했습니다."); location.href="index.php"</script> -->
+            <script>alert("로그인에 성공했습니다."); location.href="index.php"</script>
             <? session_start();
             $_SESSION['login_id'] = $mb_id;
-            echo $_SESSION['login_id'];
         } else {?>
             <script>alert("로그인에 실패했습니다. 다시 한 번 확인해주세요."); history.back();</script>
             <?} 
