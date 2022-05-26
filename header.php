859b174dd20d575eb4f48970afd80ca12d8c4c83
sunho <tjsgh9414@naver.com>
2022-05-26 22:02:11 +0900

    헤더 공통파일

--- header.php
+++ header.php
@@ -1,2 +1,14 @@
-<td>d</td>
-<td>d</td>
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <table class="main_header">
+        <tr>
+            <!-- <td><input type="button" value="자유게시판" onclick="location.href='index.php'"></td> -->
+            <td><a href="index.php">자유게시판 | </a></td>
+            <td><a href="rank.php"> &nbsp;랭킹&nbsp;| </a></td>
+        </tr>
+    </table>
+</body>
+</html>
