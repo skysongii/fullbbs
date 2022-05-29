dbea777c1e9984b0ef8be489980ccebe5e5e81f8
sunho <tjsgh9414@naver.com>
2022-05-29 23:24:06 +0900

    하늘이 사진 전용페이지

---
+++ sky.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<head>
+    <h1><center>하늘이</center></h1>
+    <meta charset="utf-8">
+    <title>하늘이</title>
+</head>
+<?php
+    include_once $_SERVER['DOCUMENT_ROOT']."/new_bbs/header.php";
+
+?>
+<body>
+    <img src="images/test.jpg" alt="test Image">
+</body>
