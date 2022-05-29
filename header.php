10bcb376c65ff251df54f3ce77d80f519b754a6e
sunho <tjsgh9414@naver.com>
2022-05-29 23:23:03 +0900

    헤더파일 수정

--- header.php
+++ header.php
@@ -6,8 +6,7 @@
     <table class="main_header">
         <tr>
             <!-- <td><input type="button" value="자유게시판" onclick="location.href='index.php'"></td> -->
-            <td><a href="index.php">자유게시판 | </a></td>
-            <td><a href="rank.php"> &nbsp;랭킹&nbsp;| </a></td>
+            <td><a href="index.php">자유게시판</a> | <a href="rank.php">랭킹</a> | <a href="sky.php">하늘이</a> | <a href="sky.php">송이</a> | <a href="sky.php">또봉이</a> | <a href="sky.php">몬순이</a></td>
         </tr>
     </table>
 </body>
