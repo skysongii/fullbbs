4b544547dc82579c314498d9e887e74f7cffc8ea
sunho <tjsgh9414@naver.com>
2022-05-30 21:02:33 +0900

    헤더 링크 수정

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <table class="main_header">
         <tr>
             <!-- <td><input type="button" value="자유게시판" onclick="location.href='index.php'"></td> -->
-            <td><a href="index.php">자유게시판</a> | <a href="rank.php">랭킹</a> | <a href="sky.php">하늘이</a> | <a href="sky.php">송이</a> | <a href="sky.php">또봉이</a> | <a href="sky.php">몬순이</a></td>
+            <td><a href="index.php">자유게시판</a> | <a href="rank.php">랭킹</a> | <a href="sky.php">하늘이</a> | <a href="song.php">송이</a> | <a href="ddobong.php">또봉이</a> | <a href="monsun.php">몬순이</a></td>
         </tr>
     </table>
 </body>
