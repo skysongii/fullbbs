9b18e3956b578321f0d6db254934e648caea57db
최선호 <93146222+skysongii@users.noreply.github.com>
2023-03-17 13:39:37 +0900

    Update common.php

--- common.php
+++ common.php
@@ -2,7 +2,7 @@
     session_start();
 	$ses_id = $_SESSION['login_id'];
     $seq = $_SESSION['seq'];
-    $password = 'gksmfs0ng@'
+    $password = '하느리송이'
 ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
